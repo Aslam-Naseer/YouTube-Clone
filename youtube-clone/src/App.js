d299087e1ada4f10a888ea1ce386b84652c6d138
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-05-27 14:27:43 +0530

    Setup Routing

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./Components";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />;
+          <Route path="/channel/:id" element={<ChannelDetail />} />;
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />;
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
 };
 
 export default App;
