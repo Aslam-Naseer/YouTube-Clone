a7af4a53d1bf4d79bfd22f888e65b470edfba80d
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-05-28 10:37:26 +0530

    Add SearchFeed

--- youtube-clone/src/Components/SearchFeed.js
+++ youtube-clone/src/Components/SearchFeed.js
@@ -1,7 +1,29 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
+
+import { fetchApi } from "../utils/fetchApi";
+import { Videos } from "./";
 
 const SearchFeed = () => {
-  return <div>SearchFeed</div>;
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    fetchApi(`search?q=${searchTerm}`).then((data) => {
+      setVideos(data.items);
+    });
+  }, [searchTerm]);
+
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        Search Result for <span style={{ color: "#FC1503" }}>{searchTerm}</span>{" "}
+        videos
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
 };
 
 export default SearchFeed;
